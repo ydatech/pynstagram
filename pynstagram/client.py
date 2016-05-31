0e7c95ffd96632815fa5ba519bc1167817f796df
Yuda Sukmana <yudasukmana1992@gmail.com>
2016-05-31 11:16:30 +0700

    Modify library for Sociocaster purpose

--- pynstagram/client.py
+++ pynstagram/client.py
@@ -1,17 +1,22 @@
 from pynstagram.session import PynstagramSession
-
-
+import json
+	
 class PynstagramClient(object):
-    def __init__(self, username, password):
-        self._session = PynstagramSession()
-        self._session.login(username, password)
-
-    def __enter__(self):
-        return self
-
-    def __exit__(self, exc_type, exc_val, exc_tb):
-        pass
-
-    def upload(self, path, caption=''):
-        media_id = self._session.upload_photo(path)
-        self._session.configure_photo(media_id, caption)
+	def __init__(self, username, password ,proxies):
+		self._session = PynstagramSession()
+		self._userlogin = self._session.login(username, password, proxies)
+	
+	def __enter__(self):
+		return self
+	
+	def __exit__(self, exc_type, exc_val, exc_tb):
+		pass
+	
+	def upload(self, path, caption = ''):
+		media_id = self._session.upload_photo(path)
+		configure_photo = self._session.configure_photo(media_id, caption)
+		
+		return configure_photo
+	
+	def userInfo(self):
+		return self._userlogin
